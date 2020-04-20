29a32c8371a644da0f8b3216ccd39b0b557b8626
applego <applegorillappa@gmail.com>
2020-04-20 15:25:58 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -35,8 +35,28 @@ for (let i = 0; i <= length; i++) {
 
 let end2 = new Date();
 
+//Mapを使ってみよう
+const memo = new Map();
+memo.set(0, 0);
+memo.set(1, 1);
+function fib_map(n) {
+    if (memo.has(n)) {
+        return memo.get(n);
+    }
+    const value = fib_map(n - 1) + fib_map(n - 2);
+    memo.set(n, value);
+    return value;
+}
+for (let i = 0; i <= length; i++){
+    console.log(i + ' ' + fib_map(i));
+}
+
+let end3 = new Date();
+
 // time node app.js で可能
 let ms1 = end1.getTime() - start.getTime();
 console.log(ms1);
 let ms2 = end2.getTime() - end1.getTime();
 console.log(ms2);
+let ms3 = end3.getTime() - end2.getTime();
+console.log(ms3);
