2f479d525a9aa4612aaaadcd88833d922da6ac6d
applego <applegorillappa@gmail.com>
2020-04-20 15:38:30 +0900

    トリボナッチ数列追加

--- app.js
+++ app.js
@@ -53,6 +53,33 @@ for (let i = 0; i <= length; i++){
 
 let end3 = new Date();
 
+
+//練習　トリボナッチ数列
+/*
+F0 = 0
+F1 = 0
+F2 = 1
+Fn+3 = Fn + Fn+1 + Fn+2 (n ≧ 0)
+*/
+const mapTri = new Map();
+mapTri.set(0, 0);
+mapTri.set(1, 0);
+mapTri.set(2, 1);
+
+function tri(n) {
+    if (mapTri.has(n)) {
+        return mapTri.get(n);
+    }
+    const value = tri(n - 3) + tri(n - 2) + tri(n - 1);
+    mapTri.set(n, value);
+    return value;
+}
+for (let i = 0; i <= length; i++) {
+    console.log(i + ' ' + tri(i));
+}
+
+let end4 = new Date();
+
 // time node app.js で可能
 let ms1 = end1.getTime() - start.getTime();
 console.log(ms1);
@@ -60,3 +87,5 @@ let ms2 = end2.getTime() - end1.getTime();
 console.log(ms2);
 let ms3 = end3.getTime() - end2.getTime();
 console.log(ms3);
+let ms4 = end4.getTime() - end3.getTime();
+console.log(ms4);
